9ea3a08fa3e253a78bfc9a10d5bbab985cf51a81
SasukeUchiha7 <apil07cr@gmail.com>
2021-04-26 20:22:25 +0530

    added tile style

--- src/components/HospitalListItem.js
+++ src/components/HospitalListItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HospitalListItem = ({ name }) => (
    // <Link className="list-item" to={`/edit/${id}`}>
-    <div>
+    <div className="list_card">
         <div>
             <h3 className="list-item__title">{name}</h3>
         </div>
