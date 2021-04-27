bf7f22afe5a51afdc219a7ff9a68ffc062ddbdaa
SasukeUchiha7 <apil07cr@gmail.com>
2021-04-27 16:20:36 +0530

    fixed styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,9 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Healthcare</h1>
+      <div><h1 className="box-layout__title">Healthcare</h1></div>
       <p>Better to wear a mask than a ventilator... Better to stay at home than in an ICU.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <button className="button login" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
 );
